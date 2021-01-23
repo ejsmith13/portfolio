de4b6296a64034ac0be70ec5b2df1cda104afc5a
ejsmith13 <ejsmithwork@gmail.com>
2021-01-23 08:22:33 -0500

    linked buttons to github repos

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-type =
-  "text/javascript" >
-  $("#refactorBtn").on("click", function () {
-    console.log("refactor button");
-    location.href = "https://github.com/ejsmith13/horiseon_marketing_project";
-  });
+$("#refactorBtn").on("click", function () {
+  console.log("refactor button");
+  location.href = "https://github.com/ejsmith13/horiseon_marketing_project";
+});
 $("#passwordBtn").on("click", function () {
   console.log("password button");
   location.href = "https://github.com/ejsmith13/password-maker";
+});
+$("#plannerBtn").on("click", function () {
+  console.log("password button");
+  location.href = "https://github.com/ejsmith13/day_planner";
+});
+$("#holidayBtn").on("click", function () {
+  console.log("password button");
+  location.href = "https://github.com/vpatt1031/movie_roullette";
+});
+$("#quizBtn").on("click", function () {
+  console.log("password button");
+  location.href = "https://github.com/ejsmith13/coding_quiz";
 });
