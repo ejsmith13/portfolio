64447182bff855f052b6135a6eda576577dfab9a
ejsmith13 <ejsmithwork@gmail.com>
2021-01-23 08:11:17 -0500

    added new pictures for projects, buttons and linked to deployed site

---
+++ script.js
@@ -0,0 +1,10 @@
+type =
+  "text/javascript" >
+  $("#refactorBtn").on("click", function () {
+    console.log("refactor button");
+    location.href = "https://github.com/ejsmith13/horiseon_marketing_project";
+  });
+$("#passwordBtn").on("click", function () {
+  console.log("password button");
+  location.href = "https://github.com/ejsmith13/password-maker";
+});
